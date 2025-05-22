432a24011a03019c91218124cc89427540049545
Ram <Ram@H.p.com>
2025-05-22 19:18:35 +0530

    modifies js and package.json file

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 var bandName = "";
 var password = "ILoveProgramming";
